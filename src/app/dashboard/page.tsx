7f0822008f0eb96adf4102195e8107bee08c0d69
Clyde95 <clydekambamura@icloud.com>
2023-11-16 14:38:00 +0200

    Done with setting up the backend and Providers.ts

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,11 @@
-import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/dist/server"
+import  {getKindeServerSession}  from "@kinde-oss/kinde-auth-nextjs/server"
 import { redirect } from "next/navigation"
 
 const page = () => {
     const {getUser} = getKindeServerSession()
     const user = getUser()
 
-    if (!user || !user.id) redirect
+    if (!user || !user.id) redirect('/auth-callback?origin=dashboard')
 
     return <div>{user.email}</div>
   
